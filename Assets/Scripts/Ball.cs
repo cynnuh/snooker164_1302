d45e2c6f026bf702033e87d2c9658b8564357c88
cynnuh <dnlap099@gmail.com>
2026-08-14 18:24:43 +0700

    8 Balls

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum BallColor
+{
+    White,
+    Red,
+    Yellow,
+    Green,
+    Brown,
+    Blue,
+    Pink,
+    Black
+}
+
+public class Ball : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    private int point;
+
+    [SerializeField]
+    private BallColor color;
+
+
+    [SerializeField]
+    private MeshRenderer rd;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log(point);
+        GameManager.instance.PlayerScore += point;
+        Destroy(gameObject);
+    }
+
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetColorAndPoint(BallColor col)
+    {
+        switch (col)
+        {
+            case BallColor.White:
+                point = 1;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = Color.brown;
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = Color.pink;
+                break;
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+        }
+    }
+}
